c082a66607521e57d2e38a086db0c41d62af6994
Gustavo Lovera <gustavolovera10@gmail.com>
2021-04-05 00:38:36 -0300

    Ajustes no modal

--- src/components/atoms/aModal/index.js
+++ src/components/atoms/aModal/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
-import { View, Text, Alert, Modal, TouchableHighlight } from 'react-native'
+import { View, Text, Modal, TouchableOpacity } from 'react-native'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { setModalVisibility } from '../../../redux/actions/index.js'
+
+import SVGInfo from '../../../../assets/icons/info.svg'
 
 import styles from './styles'
 
@@ -12,28 +14,25 @@ class AModal extends Component {
     return (
       <View style={ styles.centeredView }>
         <Modal
-          animationType="fade"
+          animationType="slide"
           transparent={ true }
           visible={ isVisible }
-          onRequestClose={() => Alert.alert('Modal has been closed.') }>
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <Text style={styles.modalText}>Hello World!</Text>
-
-              <TouchableHighlight
-                style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+          presentationStyle="overFullScreen"
+        >
+          <View style={ styles.centeredView }>
+            <SVGInfo style={ styles.infoIcon } width="48" height="48" />
+            <View style={ styles.modalView }>
+              <Text style={ styles.title }>Pilates</Text>
+              <Text style={ styles.description }>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</Text>
+              <Text style={ styles.time }>Duração: 4 minutos</Text>
+              <TouchableOpacity
+                style={ styles.closeButton }
                 onPress={() => setModalVisibility(false) }>
-                <Text style={styles.textStyle}>Hide Modal</Text>
-              </TouchableHighlight>
+                <Text style={ styles.textStyle }>Entendi</Text>
+              </TouchableOpacity>
             </View>
           </View>
         </Modal>
-
-        <TouchableHighlight
-          style={ styles.openButton }
-          onPress={() => setModalVisibility(true) }>
-          <Text style={ styles.textStyle }>Show Modal</Text>
-        </TouchableHighlight>
       </View>
     )
   }
--- src/components/atoms/aModal/styles.js
+++ src/components/atoms/aModal/styles.js
@@ -3,39 +3,44 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
+    justifyContent: 'flex-end',
   },
   modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+    backgroundColor: '#f7f7f7',
+    padding: 48,
   },
-  openButton: {
-    backgroundColor: '#F194FF',
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
+  infoIcon: {
+    marginLeft: 20,
+    marginBottom: -24,
+    zIndex: 1
   },
   textStyle: {
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  modalText: {
+  title: {
     marginBottom: 15,
-    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  description: {
+    textAlign: 'justify',
+    marginBottom: 30
+  },
+  time: {
+    marginBottom: 30
+  },
+  closeButton: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F78E00',
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingBottom: 10,
+    paddingTop: 10,
+    borderRadius: 30
   }
 })
 
