3963581cdb5a70dd82c86074db616e87aa2ea0ed
Gustavo Lovera <gustavolovera10@gmail.com>
2021-04-10 01:27:44 -0300

    Ajustes no fluxo do timer

--- src/components/molecules/mTimer/index.js
+++ src/components/molecules/mTimer/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Animated } from 'react-native'
+import { View, Animated, Modal, Text, TouchableOpacity } from 'react-native'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { setTimer } from '../../../redux/actions/index.js'
@@ -12,9 +12,17 @@ import ATitle from '../../atoms/aTitle/index.js'
 import AButton from '../../atoms/aButton/index.js'
 
 class MTimer extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isVisible: false,
+      isPlaying: true
+    }
+  }
   render() {
     return (
-      <View style={ styles.mTimer }>
+      <View style={ this.state.isVisible ? styles.mTimerOutFocus : styles.mTimer }>
         <ATitle
           style={ styles.mTitle }
           title={ this.props.exercise.name }
@@ -25,7 +33,7 @@ class MTimer extends Component {
             size={ 320 }
             trailStrokeWidth={ 8 }
             strokeWidth={ 10 }
-            isPlaying
+            isPlaying={ this.state.isPlaying }
             duration={ this.props.exercise.duration }
             colors="#fff"
             onComplete={() => {
@@ -35,22 +43,45 @@ class MTimer extends Component {
             {({ remainingTime, animatedColor }) => (
               <View style={ styles.insideCircle }>
                 <Animated.Text
-                  style={ styles.remainingTime }>
-                  { remainingTime }
+                  style={ this.state.isPlaying ? styles.remainingTime : styles.stopedTime }>
+                  { this.state.isPlaying ? remainingTime : 'Pausado' }
                 </Animated.Text>
                 <Animated.Text
                   style={ styles.secondsText }>
-                  segundos
+                  { this.state.isPlaying ? 'segundos' : '' }
                 </Animated.Text>
               </View>
             )}
           </CountdownCircleTimer>
         </View>
+        <Modal
+          animationType="slide"
+          transparent={ true }
+          visible={ this.state.isVisible }
+        >
+          <View style={ styles.centeredView }>
+            <View style={ styles.modalView }>
+              <Text style={ styles.question }>Quer mesmo parar?</Text>
+              <View style={ styles.actionButtons }>
+                <AButton
+                  text="Voltar"
+                  type="link"
+                  onPress={ () => this.setState({ isVisible: false, isPlaying: true }) }
+                />
+                <AButton
+                  text="Quero"
+                  color="white"
+                  backgroundColor="#F78E00"
+                />
+              </View>
+            </View>
+          </View>
+        </Modal>
         <AButton
           text="Parar"
           color="black"
           backgroundColor="white"
-          onPress={ () => console.log(this.props.exercise) }
+          onPress={ () => this.setState({ isVisible: true, isPlaying: false }) }
         />
       </View>
     )
--- src/components/molecules/mTimer/styles.js
+++ src/components/molecules/mTimer/styles.js
@@ -14,6 +14,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F78E00'
   },
+  mTimerOutFocus: {
+    paddingBottom: 64,
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 64,
+    flex: 1,
+    backgroundColor: '#F78E00',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -39,6 +47,71 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     color: '#787878'
+  },
+  stopedTime: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#787878'
+  },
+
+  centeredView: {
+    height: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  modalView: {
+    backgroundColor: '#f7f7f7',
+    padding: 48,
+    borderTopLeftRadius: 18,
+    borderTopRightRadius: 18,
+    shadowColor: '#000',
+    shadowOffset: { width: 1000, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  infoIcon: {
+    marginLeft: 20,
+    marginBottom: -24,
+    zIndex: 1
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  title: {
+    marginBottom: 15,
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  description: {
+    marginBottom: 30
+  },
+  time: {
+    marginBottom: 30
+  },
+  question: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 64,
+    color: '#787878'
+  },
+  closeButton: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F78E00',
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingBottom: 10,
+    paddingTop: 10,
+    borderRadius: 30
+  },
+  actionButtons: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   }
 })
 
